f5b999eca19e20e46a6dfaab193192084b1ad3e4
Tryhear <tryhear@qq.com>
2019-10-02 15:07:51 +0800

    very good!

--- bak/qw/quwu2.py
+++ bak/qw/quwu2.py
@@ -28,6 +28,7 @@ def baweraopen(image,size):
 
 def CompressImage(image_name):
     os.system("magick %s -resize 2480x3508! %s" % (image_name,image_name))
+    #os.system("magick %s -resize 1654x2339! %s" % (image_name,image_name))
     print(image_name," resized")
 
 rootdir = ".\\"
@@ -41,29 +42,38 @@ for parent,dirnames,filenames in os.walk(rootdir):
             bg=np.ones(img.shape).astype(np.uint8)
 
             img_mask=cv2.bitwise_not(cv2.imread(apath,0))
+            img_mask=baweraopen(img_mask,400)
             
             shape=img_mask.shape
+            if shape[0]>3690 or shape[1]>3690:
+                print('too big!',shape)
+                continue
             w=int(shape[1]*0.6)
             h=int(shape[0]*0.6)
-
             #print(shape,w,h)
+            
             p1=((int((shape[1]-w)/2)),int((shape[0]-h)/2))
             p2=((int((shape[1]/2)+(w/2)),int((shape[0]-h)/2)))
             p3=((int((shape[1]/2)+(w/2)),int((shape[0]/2)+(h/2))))
             p4=((int((shape[1]-w)/2),int((shape[0]/2)+(h/2))))
             points=[p1,p2,p3,p4]
+            kernel_o = np.ones((1, 1), np.uint8)
+            #kernel_c = np.ones((2, 2), np.uint8)
             #print(points)
             
-            img_mask=baweraopen(img_mask,400)
-            
-            #img_mask=cv2.fillPoly(img_mask,[np.array(points)],[255,255,255])
+            img_mask=cv2.fillPoly(img_mask,[np.array(points)],[255,255,255])
             img_mask=img_mask.astype(np.uint8)
-            ret,mask=cv2.threshold(img_mask,35,255,cv2.THRESH_BINARY)
-            cv2.imwrite("xxx.jpg",img_mask)
-            mask=cv2.bitwise_not(mask)
+            ret,img_mask=cv2.threshold(img_mask,35,255,cv2.THRESH_BINARY)
+            img_mask = cv2.morphologyEx(img_mask, cv2.MORPH_OPEN, kernel_o)
+            mask=cv2.bitwise_not(img_mask)
+            #mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel_c)
+            #cv2.imwrite(apath+"_mask.jpg",mask)
 
             dst=cv2.bitwise_or(img,bg,mask=mask)
-            dst=cv2.add(dst,img)
+            ret,dst=cv2.threshold(dst,40,255,cv2.THRESH_BINARY)
+            #cv2.imwrite(apath+"_dst.jpg",dst)
+            dst=cv2.add(img,dst)
+
             
             b, g, r = cv2.split(dst)
             r[r > 220] = 255
@@ -74,8 +84,8 @@ for parent,dirnames,filenames in os.walk(rootdir):
             r = cv2.equalizeHist(r)
             O = cv2.merge([b,g,r])
             
-            #cv2.imwrite(apath+"_.jpg",O)
-            cv2.imwrite(apath,dst)
+            cv2.imwrite(apath+"_.jpg",O)
+            #cv2.imwrite(apath,dst)
             #cv2.imwrite(apath.split('\\')[-1].split('.')[0]+'_.jpg',dst)
         else:
             pass
