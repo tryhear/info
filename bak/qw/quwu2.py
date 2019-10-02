362a547a49a435ff0d0d053665d177adfc6473e4
Tryhear <tryhear@qq.com>
2019-10-02 19:41:48 +0800

    ooooo

--- bak/qw/quwu2.py
+++ bak/qw/quwu2.py
@@ -46,45 +46,80 @@ for parent,dirnames,filenames in os.walk(rootdir):
             
             shape=img_mask.shape
             if shape[0]>3690 or shape[1]>3690:
-                print('too big!',shape)
-                continue
-            w=int(shape[1]*0.6)
-            h=int(shape[0]*0.6)
-            #print(shape,w,h)
+                w=int(shape[1]*0)
+                h=int(shape[0]*0)
+            else:
+                w=int(shape[1]*0.6)
+                h=int(shape[0]*0.6)
+            #print("h: ",shape[0],"w: ",shape[1])
             
             p1=((int((shape[1]-w)/2)),int((shape[0]-h)/2))
             p2=((int((shape[1]/2)+(w/2)),int((shape[0]-h)/2)))
             p3=((int((shape[1]/2)+(w/2)),int((shape[0]/2)+(h/2))))
             p4=((int((shape[1]-w)/2),int((shape[0]/2)+(h/2))))
             points=[p1,p2,p3,p4]
-            kernel_o = np.ones((1, 1), np.uint8)
-            #kernel_c = np.ones((2, 2), np.uint8)
-            #print(points)
+            
+            p1_top=(0,0)
+            p2_top=(int(shape[1]),0)
+            p3_top=(int(shape[1]),int(shape[0]*0.04))
+            p4_top=(0,int(shape[0]*0.04))
+            points_top=[p1_top,p2_top,p3_top,p4_top]
+            #print(points_top)
+            
+            p1_left=(0,0)
+            p2_left=(int(shape[1]*0.03),0)
+            p3_left=(int(shape[1]*0.03),int(shape[0]))
+            p4_left=(0,int(shape[0]))
+            points_left=[p1_left,p2_left,p3_left,p4_left]
+            #print(points_left)
+            
+            p1_right=(int(shape[1]*0.97),0)
+            p2_right=(int(shape[1]),0)
+            p3_right=(int(shape[1]),int(shape[0]))
+            p4_right=(int(shape[1]*0.97),int(shape[0]))
+            points_right=[p1_right,p2_right,p3_right,p4_right]
+            #print(points_right)
+            
+            p1_button=(0,int(shape[0]*0.97))
+            p2_button=(int(shape[1]),int(shape[0]*0.97))
+            p3_button=(int(shape[1]),int(shape[0]))
+            p4_button=(0,int(shape[0]))
+            points_button=[p1_button,p2_button,p3_button,p4_button]
+            #print(points_button)
             
             img_mask=cv2.fillPoly(img_mask,[np.array(points)],[255,255,255])
             img_mask=img_mask.astype(np.uint8)
             ret,img_mask=cv2.threshold(img_mask,35,255,cv2.THRESH_BINARY)
+            kernel_o = np.ones((1, 1), np.uint8)
             img_mask = cv2.morphologyEx(img_mask, cv2.MORPH_OPEN, kernel_o)
             mask=cv2.bitwise_not(img_mask)
-            #mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel_c)
+            
             #cv2.imwrite(apath+"_mask.jpg",mask)
 
             dst=cv2.bitwise_or(img,bg,mask=mask)
             ret,dst=cv2.threshold(dst,40,255,cv2.THRESH_BINARY)
             #cv2.imwrite(apath+"_dst.jpg",dst)
             dst=cv2.add(img,dst)
+            
 
-            
-            b, g, r = cv2.split(dst)
-            r[r > 220] = 255
-            b[b > 220] = 255
-            g[g > 220] = 255
-            b = cv2.equalizeHist(b)
-            g = cv2.equalizeHist(g)
-            r = cv2.equalizeHist(r)
-            O = cv2.merge([b,g,r])
-            
-            cv2.imwrite(apath+"_.jpg",O)
+            if shape[0]>3690 or shape[1]>3690:
+                pass
+            else:
+                dst=cv2.fillPoly(dst,[np.array(points_top)],[255,255,255])
+                dst=cv2.fillPoly(dst,[np.array(points_left)],[255,255,255])
+                dst=cv2.fillPoly(dst,[np.array(points_right)],[255,255,255])
+                dst=cv2.fillPoly(dst,[np.array(points_button)],[255,255,255])
+                '''
+                b, g, r = cv2.split(dst)
+                r[r > 200] = 255
+                b[b > 200] = 255
+                g[g > 200] = 255
+                b = cv2.equalizeHist(b)
+                g = cv2.equalizeHist(g)
+                r = cv2.equalizeHist(r)
+                dst = cv2.merge([b,g,r])
+                '''
+            cv2.imwrite(apath+"_.jpg",dst)
             #cv2.imwrite(apath,dst)
             #cv2.imwrite(apath.split('\\')[-1].split('.')[0]+'_.jpg',dst)
         else:
