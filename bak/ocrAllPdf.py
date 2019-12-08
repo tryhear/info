ffbd5fcec2fccbcb3a3884beeafcd0d147161a54
Tryhear <tryhear@qq.com>
2019-12-08 20:09:13 +0800

    ocrAllPdf.py add changeTime

--- bak/ocrAllPdf.py
+++ bak/ocrAllPdf.py
@@ -1,9 +1,29 @@
 import os
 import os.path
 import time
+import math
 
 #sudo apt install ocrmypdf
 #sudo apt install tesseract-ocr-chi-sim
+
+
+def changeTime(allTime):
+    day = 24*60*60
+    hour = 60*60
+    min = 60
+    if allTime <60:        
+        return  "%d 秒"%math.ceil(allTime)
+    elif  allTime > day:
+        days = divmod(allTime,day) 
+        return "%d 天, %s"%(int(days[0]),changeTime(days[1]))
+    elif allTime > hour:
+        hours = divmod(allTime,hour)
+        return '%d 小时, %s'%(int(hours[0]),changeTime(hours[1]))
+    else:
+        mins = divmod(allTime,min)
+        return "%d 分钟, %d 秒"%(int(mins[0]),math.ceil(mins[1]))
+
+
 
 start=round(time.time())
 rootdir = "./"
@@ -26,4 +46,4 @@ for parent,dirnames,filenames in os.walk(rootdir):
             os.system("ocrmypdf --output-type pdf -l chi_sim %s %s" %(thefile,thefile))
 
 elapsd=(round(time.time())-start)
-print("\n处理 %s 个文件一共耗时 %s 分钟.\n" %(count,round(elapsd/60)))
+print("\n处理 %s 个文件一共耗时 %s.\n" %(count,changeTime(elapsd)))
