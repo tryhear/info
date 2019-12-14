6f867a207d5b47d5f4f81f930abd94b7f05db52f
Tryhear <tryhear@qq.com>
2019-12-14 11:46:43 +0800

    rmfile

--- bak/rmfile.py
+++ bak/rmfile.py
@@ -6,20 +6,26 @@ count=0
 dirs=os.listdir()
 for dir in dirs:
     path=os.path.join(os.getcwd(),dir)
-    #print(path)
+    path1=os.path.join(os.getcwd(),dir,dir,dir)
+
     if not os.path.isfile(path):
         try:
-            #os.remove(path)
-            os.system("del %s\\*.* /q" % path)
-            #print(path)
             if os.path.isdir(os.path.join(path, dir,dir)):
                 pass
             else:
                 print(path)
                 count+=1
+                print(path+"error!!!")
+                #input()
             #os.system("cd %s\\%s\\%s" %(dir,dir,dir))
+
+            #os.system("del %s\\*.* /q" % path)
+            os.system("move /Y %s\\*.* %s" %(path1,path))
+            shutil.rmtree(os.path.join(os.getcwd(),dir,dir))
+
         except:
             traceback.print_exc()
     else:
         pass
 print(count)
+input()
